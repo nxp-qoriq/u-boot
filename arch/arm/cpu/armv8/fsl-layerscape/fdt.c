nxp-qoriq/u-boot

commit e0369e0e1b1f491cbd9199ceb3901dfc1b4d90f2

armv8/fsl-layerscape: fdt: remove SYSCLK frequency fixup for ls1012a
Generally SYSCLK frequency is dependent on on-board switch settings.
It may vary as per requirement, but this doesn't apply to ls1012a.
ls1012a has its SYSCLK frequencies specified in the RM. The fixup
for all 'fixed-clock' compatibles of ls1012a would cause incorrect
SYSCLK frequency values. So remove the SYSCLK frequency fixup for ls1012a.

Fixes: 6f14e25 ("armv8: fsl-lsch3: fixup SYSCLK frequency in device tree")
Signed-off-by: Yangbo Lu <yangbo.lu@nxp.com>

arch/arm/cpu/armv8/fsl-layerscape/fdt.c
@@ -299,8 +299,10 @@ void ft_cpu_setup(void *blob, bd_t *bd)
 			       "clock-frequency", CONFIG_SYS_NS16550_CLK, 1);
 #endif
 
+#ifndef CONFIG_ARCH_LS1012A
 	do_fixup_by_compat_u32(blob, "fixed-clock",
 			       "clock-frequency", CONFIG_SYS_CLK_FREQ, 1);
+#endif
 
 #ifdef CONFIG_PCI
 	ft_pci_setup(blob, bd);

commit f4ea06d7f35032c7877126cb3ec443b480871e5c

armv8/fsl-layerscape: fdt: replace CONFIG_ARCH_LS1012A with CONFIG_LS1012A
Upstream u-boot uses CONFIG_ARCH_LS1012A while our sdk uses CONFIG_LS1012A.
This patch is to replace CONFIG_ARCH_LS1012A with CONFIG_LS1012A to avoid
clock issue.

Fixes: e0369e0e1b1f ("armv8/fsl-layerscape: fdt: remove SYSCLK frequency
       fixup for ls1012a")
Signed-off-by: Yangbo Lu <yangbo.lu@nxp.com>

arch/arm/cpu/armv8/fsl-layerscape/fdt.c
@@ -299,7 +299,7 @@ void ft_cpu_setup(void *blob, bd_t *bd)
 			       "clock-frequency", CONFIG_SYS_NS16550_CLK, 1);
 #endif
 
-#ifndef CONFIG_ARCH_LS1012A
+#ifndef CONFIG_LS1012A
 	do_fixup_by_compat_u32(blob, "fixed-clock",
 			       "clock-frequency", CONFIG_SYS_CLK_FREQ, 1);
 #endif