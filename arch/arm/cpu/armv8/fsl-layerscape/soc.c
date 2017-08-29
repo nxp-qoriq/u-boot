46526a05629ff2eda4c11d565296d5cffbe0b2ab
Prabhakar Kushwaha <prabhakar.kushwaha@nxp.com>
2017-08-29 16:07:59 +0530

    armv8: fsl-layerscape: SPL size reduction
    Using changes in this patch we were able to reduce approx 4k
    size of u-boot-spl.bin image. Following is breif description of
    changes to reduce SPL size:
    1. Compile-off mp.c and libfdt.c in case of SPL build.
    2. Keep MMU and DCACHE specific variable and functions under
       CONFIG_SYS_DCACHE_OFF macro.
    3. Compile-off IFC specific funtion call "init_early_memctl_regs"
       in case of SPL build.
    
    Signed-off-by: Sumit Garg <sumit.garg@nxp.com>

--- arch/arm/cpu/armv8/fsl-layerscape/soc.c
+++ arch/arm/cpu/armv8/fsl-layerscape/soc.c
@@ -329,7 +329,9 @@ void bypass_smmu(void)
 void fsl_lsch3_early_init_f(void)
 {
 	erratum_rcw_src();
+#ifndef CONFIG_SPL_BUILD
 	init_early_memctl_regs();	/* tighten IFC timing */
+#endif
 #ifdef CONFIG_SYS_FSL_ERRATUM_A009203
 	erratum_a009203();
 #endif
