503eff00a7119d2690a662ed64e83e4782b7d3d9
Gong Qianyu <Qianyu.Gong@nxp.com>
2017-09-18 16:24:16 +0800

    armv8: ls1046aqds: Fix NAND offset for Fman ucode and env
    Fix a bug of 'commit 8104deb2d6b7 ("armv8: layerscape: Adjust memory
    mapping for Flash/SD card on LS1046A")' as NAND block size is
    256KB on LS1046AQDS.
    
    Signed-off-by: Gong Qianyu <Qianyu.Gong@nxp.com>

--- include/configs/ls1046aqds.h
+++ include/configs/ls1046aqds.h
@@ -445,7 +445,7 @@ unsigned long get_board_ddr_clk(void);
 #ifdef CONFIG_NAND_BOOT
 #define CONFIG_ENV_IS_IN_NAND
 #define CONFIG_ENV_SIZE			0x2000
-#define CONFIG_ENV_OFFSET		(24 * CONFIG_SYS_NAND_BLOCK_SIZE)
+#define CONFIG_ENV_OFFSET		(12 * CONFIG_SYS_NAND_BLOCK_SIZE)
 #elif defined(CONFIG_SD_BOOT)
 #define CONFIG_ENV_OFFSET		(3 * 1024 * 1024)
 #define CONFIG_ENV_IS_IN_MMC
