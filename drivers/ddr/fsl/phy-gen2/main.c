595ebb4e9bcdf9c6822bd2deb8ff5ec70db1b24b
Rajesh Bhagat <rajesh.bhagat@nxp.com>
2018-04-05 04:50:58 +0530

    arm/lx2160a: Add support for DDR PHY debugging path
    Signed-off-by: Pankit Garg <pankit.garg@nxp.com>

--- drivers/ddr/fsl/phy-gen2/main.c
+++ drivers/ddr/fsl/phy-gen2/main.c
@@ -19,6 +19,121 @@
 #include "include/io.h"
 #endif
 
+/* #define DEBUG */
+
+#ifdef DEBUG
+#include "include/csr.h"
+#include "include/imem.h"
+#include "include/dmem.h"
+#include "include/pie.h"
+#include "include/overrides.h"
+
+enum image_types {
+	CSR,
+	IMEM,
+	DMEM,
+	PIE,
+	OVERRIDES
+};
+
+void load_image(const unsigned int ctrl_num, enum image_types image_type)
+{
+	int i;
+	int size;
+
+	const struct csr *csr_image = NULL;
+	const struct imem *imem_image = NULL;
+	const struct dmem *dmem_image = NULL;
+	const struct pie *pie_image = NULL;
+	const struct overrides *overrides_image = NULL;
+
+	printf("Load Image-%d..\n", image_type);
+	switch (image_type) {
+	case CSR:
+		csr_image = phy_csr;
+		size = ARRAY_SIZE(phy_csr);
+		if (csr_image) {
+			for (i = 0; i < size; i++)
+				phy_io_write16(ctrl_num, csr_image[i].addr, csr_image[i].data);
+		}
+		break;
+	case IMEM:
+		imem_image = image_imem;
+		size = ARRAY_SIZE(image_imem);
+		if (imem_image) {
+			for (i = 0; i < size; i++)
+				phy_io_write16(ctrl_num, imem_image[i].addr, imem_image[i].data);
+		}
+		break;
+	case DMEM:
+		dmem_image = image_dmem;
+		size = ARRAY_SIZE(image_dmem);
+		if (dmem_image) {
+			for (i = 0; i < size; i++)
+				phy_io_write16(ctrl_num, dmem_image[i].addr, dmem_image[i].data);
+		}
+		break;
+	case PIE:
+		pie_image = image_udimm;
+		size = ARRAY_SIZE(image_udimm);
+		if (pie_image) {
+			for (i = 0; i < size; i++)
+				phy_io_write16(ctrl_num, pie_image[i].addr, pie_image[i].data);
+		}
+		break;
+	case OVERRIDES:
+		overrides_image = image_overrides;
+		size = ARRAY_SIZE(image_overrides);
+		if (overrides_image) {
+			for (i = 0; i < size; i++)
+				phy_io_write16(ctrl_num, overrides_image[i].addr, overrides_image[i].data);
+		}
+		break;
+
+	default:
+		printf("Unsupported Image type\n");
+		break;
+	}
+	printf("Load Image-%d..Done\n", image_type);
+}
+
+unsigned int compute_phy_config_regs(const unsigned int ctrl_num,
+		const memctl_options_t *popts,
+		fsl_ddr_cfg_regs_t *ddr)
+{
+	printf("DDR PHY Debug Path\n");
+
+	/* configure phy register */
+	load_image(ctrl_num, CSR);
+
+	/* Initialise firmware mailbox */
+	phy_io_write16(ctrl_num, 0x0d0031, 0x1);
+	phy_io_write16(ctrl_num, 0x0c0033, 0x1);
+
+	/* Load IMEM image */
+	load_image(ctrl_num, IMEM);
+
+	/* Load DMEM image into memory */
+	load_image(ctrl_num, DMEM);
+
+	/* Execute training firmware and Wait for training to complete */
+	g_exec_fw(ctrl_num);
+
+	/* Load Phy Init Engine image */
+	phy_io_write16(ctrl_num, 0x0d0000, 0x0);
+	load_image(ctrl_num, PIE);
+	phy_io_write16(ctrl_num, 0x0d0000, 0x1);
+
+#ifdef CONFIG_ARCH_LX2160A_PXP
+	phy_io_write16(ctrl_num, 0x0d0000, 0);
+	load_image(ctrl_num, OVERRIDES);
+	phy_io_write16(ctrl_num, 0x0d0000, 1);
+#endif
+	printf("DDR PHY Debug Path.. Done\n");
+	return 0;
+
+}
+#else
 unsigned int compute_phy_config_regs(const unsigned int ctrl_num,
 		const memctl_options_t *popts,
 		fsl_ddr_cfg_regs_t *ddr)
@@ -99,3 +214,4 @@ unsigned int compute_phy_config_regs(const unsigned int ctrl_num,
 
 	return 0;
 }
+#endif
