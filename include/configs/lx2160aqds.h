46550c62bdc8399a605c571db01a258d90767776
Wasim Khan <wasim.khan@nxp.com>
2018-05-07 10:59:46 +0530

    lx2160aqds: Enable support for EEPROM
    Enable support to read/write from EEPROM for lx2160aqds board
    
    Signed-off-by: Wasim Khan <wasim.khan@nxp.com>

--- include/configs/lx2160aqds.h
+++ include/configs/lx2160aqds.h
@@ -52,6 +52,15 @@ u8 qixis_esdhc_detect_quirk(void);
 #define CONFIG_ESDHC_DETECT_QUIRK  qixis_esdhc_detect_quirk()
 #endif
 
+/* EEPROM */
+#define CONFIG_ID_EEPROM
+#define CONFIG_SYS_I2C_EEPROM_NXID
+#define CONFIG_SYS_EEPROM_BUS_NUM	0
+#define CONFIG_SYS_I2C_EEPROM_ADDR	0x57
+#define CONFIG_SYS_I2C_EEPROM_ADDR_LEN	1
+#define CONFIG_SYS_EEPROM_PAGE_WRITE_BITS 3
+#define CONFIG_SYS_EEPROM_PAGE_WRITE_DELAY_MS 5
+
 /* MAC/PHY configuration */
 #if defined(CONFIG_FSL_MC_ENET) && !defined(CONFIG_SPL_BUILD)
 #define CONFIG_PHYLIB_10G
