319303c3415a6ab06955f7d0c1e46d825323cb14
Hou Zhiqiang <Zhiqiang.Hou@nxp.com>
2018-06-13 15:10:27 +0800

    configs/lx2160a: add the kernel parameter pcie_bus_perf
    Signed-off-by: Hou Zhiqiang <Zhiqiang.Hou@nxp.com>

--- include/configs/lx2160a_common.h
+++ include/configs/lx2160a_common.h
@@ -317,7 +317,7 @@ unsigned long get_board_ddr_clk(void);
 #define CONFIG_BOOTARGS		"console=ttyAMA0,115200 root=/dev/ram0 " \
 				"earlycon=pl011,mmio32,0x21c0000 " \
 				"ramdisk_size=0x2000000 default_hugepagesz=1024m" \
-				" hugepagesz=1024m hugepages=2"
+				" hugepagesz=1024m hugepages=2 pci=pcie_bus_perf"
 #else
 #define CONFIG_BOOTARGS		"console=ttyAMA0,115200 root=/dev/ram0 " \
 				"earlycon=pl011,mmio32,0x21c0000 " \
