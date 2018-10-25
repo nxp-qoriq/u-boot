bc1a04fbd1bc254747281fd202b7795ce864d7ed
Ran Wang <ran.wang_1@nxp.com>
2018-10-25 17:21:42 +0800

    armv8: Add workaround for USB erratum A-010078
    USB 3.0 Super Speed (SS) compliance test may fail Transmitter Far End
    RMS Random Jitter Test with default setting of MPLL_LOOP_CTL[PROP_CNTRL]
    = 1100b. These bits are supposed to be programmed for normal operation
    mode as well, since the Transmitter Far End Random Jitter impacts the
    data eye diagram.
    
    For LS1043r1, it is required to program MPLL_LOOP_CTL.PROP_CNTRL to a
    value of 4'h4 for random jitter to pass.
    
    For LS1012, it is required to program MPLL_LOOP_CTL.PROP_CNTRL to a value
    of 4'h1 for random jitter to pas
    
    Signed-off-by: Ran Wang <ran.wang_1@nxp.com>

--- arch/arm/cpu/armv8/fsl-layerscape/soc.c
+++ arch/arm/cpu/armv8/fsl-layerscape/soc.c
@@ -1,5 +1,6 @@
 /*
  * Copyright 2014-2015 Freescale Semiconductor
+ * Copyright 2017-2018 NXP
  *
  * SPDX-License-Identifier:	GPL-2.0+
  */
@@ -52,6 +53,25 @@ bool soc_has_aiop(void)
 		return true;
 
 	return false;
+}
+
+static void erratum_a010078(void)
+{
+#ifdef CONFIG_SYS_FSL_ERRATUM_A010078
+	void __iomem *usb_phy = (void __iomem *)SCFG_USB_PHY1;
+
+#if defined(CONFIG_ARCH_LS1043A)
+	out_be16(usb_phy + SCFG_USB_MPLL_LOOP_CTL, 4 << 4);
+
+	usb_phy = (void __iomem *)SCFG_USB_PHY2;
+	out_be16(usb_phy + SCFG_USB_MPLL_LOOP_CTL, 4 << 4);
+
+	usb_phy = (void __iomem *)SCFG_USB_PHY3;
+	out_be16(usb_phy + SCFG_USB_MPLL_LOOP_CTL, 4 << 4);
+#elif defined(CONFIG_ARCH_LS1012A)
+	out_be16(usb_phy + SCFG_USB_MPLL_LOOP_CTL, 1 << 4);
+#endif
+#endif
 }
 
 static inline void set_usb_txvreftune(u32 __iomem *scfg, u32 offset)
@@ -679,6 +699,7 @@ void fsl_lsch2_early_init_f(void)
 	erratum_a009798();
 	erratum_a008997();
 	erratum_a009007();
+	erratum_a010078();
 }
 #endif
 
