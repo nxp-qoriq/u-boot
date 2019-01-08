18a1f077fa11d6937f9f34a407e3848e0f423eef
Pankit Garg <pankit.garg@nxp.com>
2019-01-08 10:02:49 +0530

    lx2160a: Revise board-specific DDR setting
    Drop board specific timing settings which are no longer used for
    the new PHY. Set RTT park to 120ohms. Set burst length to 8.
    Override turnaround timings for stability.
    
    Signed-off-by: York Sun <york.sun@nxp.com>

--- board/freescale/lx2160a/ddr.c
+++ board/freescale/lx2160a/ddr.c
@@ -13,7 +13,6 @@
 #include <asm/io.h>
 #include <fsl_ddr.h>
 #endif
-#include "ddr.h"
 
 DECLARE_GLOBAL_DATA_PTR;
 
@@ -21,107 +20,22 @@ void fsl_ddr_board_options(memctl_options_t *popts,
 			   dimm_params_t *pdimm,
 			   unsigned int ctrl_num)
 {
-	const struct board_specific_parameters *pbsp, *pbsp_highest = NULL;
-	ulong ddr_freq;
-	int slot;
-
-	if (ctrl_num > CONFIG_SYS_FSL_DDR_MAIN_NUM_CTRLS) {
-		printf("Not supported controller number %d\n", ctrl_num);
-		return;
-	}
-
-	for (slot = 0; slot < CONFIG_DIMM_SLOTS_PER_CTLR; slot++) {
-		if (pdimm[slot].n_ranks)
-			break;
-	}
-
-	if (slot >= CONFIG_DIMM_SLOTS_PER_CTLR)
-		return;
-
-	/*
-	 * we use identical timing for all slots. If needed, change the code
-	 * to  pbsp = rdimms[ctrl_num] or pbsp = udimms[ctrl_num];
-	 */
-	if (popts->registered_dimm_en)
-		pbsp = rdimms[ctrl_num];
-	else
-		pbsp = udimms[ctrl_num];
-
-	/* Get clk_adjust, wrlvl_start, wrlvl_ctl, according to the board ddr
-	 * freqency and n_banks specified in board_specific_parameters table.
-	 */
-	ddr_freq = get_ddr_freq(ctrl_num) / 1000000;
-	while (pbsp->datarate_mhz_high) {
-		if (pbsp->n_ranks == pdimm[slot].n_ranks &&
-		    (pdimm[slot].rank_density >> 30) >= pbsp->rank_gb) {
-			if (ddr_freq <= pbsp->datarate_mhz_high) {
-				popts->clk_adjust = pbsp->clk_adjust;
-				popts->wrlvl_start = pbsp->wrlvl_start;
-				popts->wrlvl_ctl_2 = pbsp->wrlvl_ctl_2;
-				popts->wrlvl_ctl_3 = pbsp->wrlvl_ctl_3;
-				goto found;
-			}
-			pbsp_highest = pbsp;
-		}
-		pbsp++;
-	}
-
-	if (pbsp_highest) {
-		printf("Error: board specific timing not found for %lu MT/s\n",
-		       ddr_freq);
-		printf("Trying to use the highest speed (%u) parameters\n",
-		       pbsp_highest->datarate_mhz_high);
-		popts->clk_adjust = pbsp_highest->clk_adjust;
-		popts->wrlvl_start = pbsp_highest->wrlvl_start;
-		popts->wrlvl_ctl_2 = pbsp->wrlvl_ctl_2;
-		popts->wrlvl_ctl_3 = pbsp->wrlvl_ctl_3;
-	} else {
-		panic("DIMM is not supported by this board");
-	}
-found:
-	debug("Found timing match: n_ranks %d, data rate %d, rank_gb %d\n"
-		"\tclk_adjust %d, wrlvl_start %d, wrlvl_ctrl_2 0x%x, wrlvl_ctrl_3 0x%x\n",
-		pbsp->n_ranks, pbsp->datarate_mhz_high, pbsp->rank_gb,
-		pbsp->clk_adjust, pbsp->wrlvl_start, pbsp->wrlvl_ctl_2,
-		pbsp->wrlvl_ctl_3);
-	/* To work at higher than 1333MT/s */
-	popts->half_strength_driver_enable = 0;
-	/*
-	 * Write leveling override
-	 */
-	popts->wrlvl_override = 1;
-	popts->wrlvl_sample = 0x0;	/* 32 clocks */
-
 	/*
 	 * Rtt and Rtt_WR override
 	 */
 	popts->rtt_override = 0;
+	popts->rtt_park = 120;
 
 	/* Enable ZQ calibration */
 	popts->zq_en = 1;
 
-	/* optimize cpo for erratum A-009942 */
-	popts->cpo_sample = 0x6e;
-
-	if (ddr_freq < 2350) {
-		if (pdimm[0].n_ranks == 2 && pdimm[1].n_ranks == 2) {
-			/* four chip-selects */
-			popts->ddr_cdr1 = DDR_CDR1_DHC_EN |
-					  DDR_CDR1_ODT(DDR_CDR_ODT_80ohm);
-			popts->ddr_cdr2 = DDR_CDR2_ODT(DDR_CDR_ODT_80ohm);
-			popts->twot_en = 1;	/* enable 2T timing */
-		} else {
-			popts->ddr_cdr1 = DDR_CDR1_DHC_EN |
-					  DDR_CDR1_ODT(DDR_CDR_ODT_60ohm);
-			popts->ddr_cdr2 = DDR_CDR2_ODT(DDR_CDR_ODT_60ohm) |
-					  DDR_CDR2_VREF_RANGE_2;
-		}
-	} else {
-		popts->ddr_cdr1 = DDR_CDR1_DHC_EN |
-				  DDR_CDR1_ODT(DDR_CDR_ODT_100ohm);
-		popts->ddr_cdr2 = DDR_CDR2_ODT(DDR_CDR_ODT_100ohm) |
-				  DDR_CDR2_VREF_RANGE_2;
-	}
+	popts->otf_burst_chop_en = 0;
+	popts->burst_length = DDR_BL8;
+	popts->trwt_override = 1;
+	popts->trwt = 0x3;
+	popts->twrt = 0x3;
+	popts->trrt = 0x3;
+	popts->twwt = 0x3;
 }
 
 #if defined(CONFIG_ARCH_LX2160A_PXP) || defined(CONFIG_DDR_FIXED_SETTINGS)
