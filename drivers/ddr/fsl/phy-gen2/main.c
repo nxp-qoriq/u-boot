48a7e7aa175dc077d6b9c89315b15c1cc4aa7663
Pankit Garg <pankit.garg@nxp.com>
2019-01-14 11:04:30 +0530

    lx2: phy-gen2: remove PHY_GEN2-PHY version info
    Signed-off-by: Pankit Garg <pankit.garg@nxp.com>

--- drivers/ddr/fsl/phy-gen2/main.c
+++ drivers/ddr/fsl/phy-gen2/main.c
@@ -142,12 +142,6 @@ unsigned int compute_phy_config_regs(const unsigned int ctrl_num,
 {
 	debug("DDR PHY Debug Path\n");
 
-#ifdef CONFIG_FSL_PHY_GEN2_PHY_A2017_11
-	printf("PHY_GEN2 FW Version: A2017.11\n");
-#else
-	printf("PHY_GEN2 FW Version: A2017.05\n");
-#endif
-
 	/* configure phy register */
 	load_image(ctrl_num, CSR);
 
@@ -271,12 +265,6 @@ unsigned int compute_phy_config_regs(const unsigned int ctrl_num,
 	size_t len;
 	int i;
 	unsigned int odt_rd, odt_wr;
-
-#ifdef CONFIG_FSL_PHY_GEN2_PHY_A2017_11
-	printf("PHY_GEN2 FW Version: A2017.11\n");
-#else
-	printf("PHY_GEN2 FW Version: A2017.05\n");
-#endif
 
 	dimm = calloc(1, sizeof(struct dimm));
 	if (!dimm) {
