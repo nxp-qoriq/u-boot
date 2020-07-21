b8905936fb609e1b8c78787ecce2dcc4c43d1605
Yangbo Lu <yangbo.lu@nxp.com>
2020-07-21 10:52:06 +0800

    Drop CONFIG_FSL_ESDHC_USE_PERIPHERAL_CLK usage
    The eSDHC reference clocks should be provided by speed.c in arch/.
    And we do not need CONFIG_FSL_ESDHC_USE_PERIPHERAL_CLK option to
    select which clock to use. Because we can make the driver to select
    the periperhal clock which is better (provides higher frequency)
    automatically if its value is provided by speed.c.
    
    This patch is to drop this option and make driver to select clock
    automatically. Also fix peripheral clock calculation issue in
    fsl_lsch2_speed.c/fsl_lsch3_speed.c.
    
    [rework]
    Signed-off-by: Yangbo Lu <yangbo.lu@nxp.com>

--- arch/arm/cpu/armv8/fsl-layerscape/fsl_lsch3_speed.c
+++ arch/arm/cpu/armv8/fsl-layerscape/fsl_lsch3_speed.c
@@ -159,14 +159,14 @@ void get_sys_info(struct sys_info *sys_info)
 		break;
 	}
 #endif
-#if defined(CONFIG_TARGET_LX2160ARDB) || defined(CONFIG_TARGET_LS2080ARDB)
-	sys_info->freq_cga_m2 = sys_info->freq_systembus;
-#endif
 }
 
 int get_clocks(void)
 {
 	struct sys_info sys_info;
+#ifdef CONFIG_FSL_ESDHC
+	u32 clock = 0;
+#endif
 	get_sys_info(&sys_info);
 	gd->cpu_clk = sys_info.freq_processor[0];
 	gd->bus_clk = sys_info.freq_systembus / CONFIG_SYS_FSL_PCLK_DIV;
@@ -174,18 +174,16 @@ int get_clocks(void)
 #ifdef CONFIG_SYS_FSL_HAS_DP_DDR
 	gd->arch.mem2_clk = sys_info.freq_ddrbus2;
 #endif
-#if defined(CONFIG_FSL_ESDHC)
-#if defined(CONFIG_FSL_ESDHC_USE_PERIPHERAL_CLK)
-#if defined(CONFIG_TARGET_LS1028ARDB) || defined(CONFIG_TARGET_LX2160ARDB)
-	gd->arch.sdhc_clk = sys_info.freq_cga_m2 / 2;
+
+#ifdef CONFIG_FSL_ESDHC
+#if defined(CONFIG_ARCH_LS1028A) || defined(CONFIG_ARCH_LS1088A)
+	clock = sys_info.freq_cga_m2;
+#elif defined(CONFIG_ARCH_LX2160A) || defined(CONFIG_ARCH_LS2080A)
+	clock = sys_info.freq_systembus;
 #endif
-#if defined(CONFIG_TARGET_LS2080ARDB) || defined(CONFIG_TARGET_LS1088ARDB)
-	gd->arch.sdhc_clk = sys_info.freq_cga_m2;
-#endif
-#else
+	gd->arch.sdhc_per_clk = clock / CONFIG_SYS_FSL_SDHC_CLK_DIV;
 	gd->arch.sdhc_clk = gd->bus_clk / CONFIG_SYS_FSL_SDHC_CLK_DIV;
 #endif
-#endif /* defined(CONFIG_FSL_ESDHC) */
 
 	if (gd->cpu_clk != 0)
 		return 0;
