079be1c1328edfbdccddc67eb207893ad3508589
Yangbo Lu <yangbo.lu@nxp.com>
2020-07-22 20:14:07 +0800

    mmc: move mmc_power_cycle() after controller initialization
    The mmc_power_cycle() should be put after controller initialization
    because the mmc_power_off() would call mmc_set_clock() which will
    configure the controller through mmc_set_ios().
    
    Signed-off-by: Yangbo Lu <yangbo.lu@nxp.com>

--- drivers/mmc/mmc.c
+++ drivers/mmc/mmc.c
@@ -2748,21 +2748,6 @@ int mmc_get_op_cond(struct mmc *mmc)
 		      MMC_QUIRK_RETRY_SEND_CID;
 #endif
 
-	err = mmc_power_cycle(mmc);
-	if (err) {
-		/*
-		 * if power cycling is not supported, we should not try
-		 * to use the UHS modes, because we wouldn't be able to
-		 * recover from an error during the UHS initialization.
-		 */
-		pr_debug("Unable to do a full power cycle. Disabling the UHS modes for safety\n");
-		uhs_en = false;
-		mmc->host_caps &= ~UHS_CAPS;
-		err = mmc_power_on(mmc);
-	}
-	if (err)
-		return err;
-
 #if CONFIG_IS_ENABLED(DM_MMC)
 	/*
 	 * Re-initialization is needed to clear old configuration for
@@ -2780,6 +2765,21 @@ int mmc_get_op_cond(struct mmc *mmc)
 retry:
 	mmc_set_initial_state(mmc);
 
+	err = mmc_power_cycle(mmc);
+	if (err) {
+		/*
+		 * if power cycling is not supported, we should not try
+		 * to use the UHS modes, because we wouldn't be able to
+		 * recover from an error during the UHS initialization.
+		 */
+		pr_debug("Unable to do a full power cycle. Disabling the UHS modes for safety\n");
+		uhs_en = false;
+		mmc->host_caps &= ~UHS_CAPS;
+		err = mmc_power_on(mmc);
+	}
+	if (err)
+		return err;
+
 	/* Reset the Card */
 	err = mmc_go_idle(mmc);
 
@@ -2796,7 +2796,6 @@ retry:
 	err = sd_send_op_cond(mmc, uhs_en);
 	if (err && uhs_en) {
 		uhs_en = false;
-		mmc_power_cycle(mmc);
 		goto retry;
 	}
 
