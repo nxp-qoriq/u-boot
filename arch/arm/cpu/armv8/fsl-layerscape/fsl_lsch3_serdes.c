03687a85e6a59b99bcb2c1bfe3bfb1b2e9d4531c
Vabhav Sharma <vabhav.sharma@nxp.com>
2020-10-08 16:20:38 +0530

    LA1224: Add support for LA1224 board
    Added support for AQR113C
    Disable SGMII 17 & 18 even if serdes protocol is enabling it
    because these are not used.
    Enable RGMII 17 in place of SGMII17 and disable RGMII18.
    Deleted unnecessary code from board file for this board
    
    This patch adds modifications used to test the board.
    It is not a final patch.
    
    Signed-off-by: Razvan Ionut Cirjan <razvanionut.cirjan@nxp.com>
    Signed-off-by: Vabhav Sharma <vabhav.sharma@nxp.com>

--- arch/arm/cpu/armv8/fsl-layerscape/fsl_lsch3_serdes.c
+++ arch/arm/cpu/armv8/fsl-layerscape/fsl_lsch3_serdes.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0+
 /*
- * Copyright 2016-2018 NXP
+ * Copyright 2016-2018,2020 NXP
  * Copyright 2014-2015 Freescale Semiconductor, Inc.
  */
 
@@ -159,11 +159,15 @@ void serdes_init(u32 sd, u32 sd_addr, u32 rcwsr, u32 sd_prctl_mask,
 			if (lane_prtcl >= XFI1 && lane_prtcl <= XFI14)
 				wriop_init_dpmac(sd, xfi_dpmac[lane_prtcl],
 						 (int)lane_prtcl);
-
+#ifdef CONFIG_TARGET_LX2160ABNYRG
+			if (lane_prtcl >= SGMII1 && lane_prtcl <= SGMII12)
+				wriop_init_dpmac(sd, sgmii_dpmac[lane_prtcl],
+						 (int)lane_prtcl);
+#else
 			if (lane_prtcl >= SGMII1 && lane_prtcl <= SGMII18)
 				wriop_init_dpmac(sd, sgmii_dpmac[lane_prtcl],
 						 (int)lane_prtcl);
-
+#endif
 			if (lane_prtcl >= _25GE1 && lane_prtcl <= _25GE10)
 				wriop_init_dpmac(sd, a25gaui_dpmac[lane_prtcl],
 						 (int)lane_prtcl);
