1aa25047edf07be7d19b8a7015e525a7efa75e38
Florin Chiculita <florinlaurentiu.chiculita@nxp.com>
2021-03-02 20:19:31 +0200

    board: fsl: la1238rdb: add ethernet initialization
    LA1238 uses FSL_MDIO0 bus having an AQR113 ethernet phy operating
    in USXGMII mode.
    
    Signed-off-by: Florin Chiculita <florinlaurentiu.chiculita@nxp.com>

--- board/freescale/lx2160a/eth_la1238rdb.c
+++ board/freescale/lx2160a/eth_la1238rdb.c
@@ -24,6 +24,52 @@ DECLARE_GLOBAL_DATA_PTR;
 
 int board_eth_init(bd_t *bis)
 {
+#if defined(CONFIG_FSL_MC_ENET)
+	struct memac_mdio_info mdio_info;
+	struct memac_mdio_controller *reg;
+	struct mii_dev *dev;
+	struct ccsr_gur *gur = (void *)(CONFIG_SYS_FSL_GUTS_ADDR);
+	u32 srds_s1;
+
+	srds_s1 = in_le32(&gur->rcwsr[28]) &
+				FSL_CHASSIS3_RCWSR28_SRDS1_PRTCL_MASK;
+	srds_s1 >>= FSL_CHASSIS3_RCWSR28_SRDS1_PRTCL_SHIFT;
+
+	reg = (struct memac_mdio_controller *)CONFIG_SYS_FSL_WRIOP1_MDIO1;
+	mdio_info.regs = reg;
+	mdio_info.name = DEFAULT_WRIOP_MDIO1_NAME;
+
+	/* Register the EMI 1 bus */
+	fm_memac_mdio_init(bis, &mdio_info);
+
+	/* LA1224 uses SerDes1 protocol 3 */
+	if (srds_s1 != 3) {
+		printf("SerDes1 protocol 0x%x is not supported by LA1224-RDB\n",
+		       srds_s1);
+		goto next;
+	}
+
+	wriop_init_dpmac_enet_if(WRIOP1_DPMAC3, PHY_INTERFACE_MODE_USXGMII);
+	wriop_set_phy_address(WRIOP1_DPMAC3, 0, AQR113_PHY_ADDR);
+	dev = miiphy_get_dev_by_name(DEFAULT_WRIOP_MDIO1_NAME);
+	wriop_set_mdio(WRIOP1_DPMAC3, dev);
+
+next:
+	cpu_eth_init(bis);
+#endif /* CONFIG_FSL_MC_ENET */
+
+#ifdef CONFIG_PHY_AQUANTIA
+	/*
+	 * Export functions to be used by AQ firmware
+	 * upload application
+	 */
+	gd->jt->strcpy = strcpy;
+	gd->jt->mdelay = mdelay;
+	gd->jt->mdio_get_current_dev = mdio_get_current_dev;
+	gd->jt->phy_find_by_mask = phy_find_by_mask;
+	gd->jt->mdio_phydev_for_ethname = mdio_phydev_for_ethname;
+	gd->jt->miiphy_set_current_dev = miiphy_set_current_dev;
+#endif
 	return pci_eth_init(bis);
 }
 
@@ -37,5 +83,8 @@ int fdt_fixup_board_phy(void *fdt)
 #if defined(CONFIG_RESET_PHY_R)
 void reset_phy(void)
 {
+#if defined(CONFIG_FSL_MC_ENET)
+	mc_env_boot();
+#endif
 }
 #endif /* CONFIG_RESET_PHY_R */
