234c5fcffdeaeab34cf61e3315581d0ed98eac3e
Yangbo Lu <yangbo.lu@nxp.com>
2021-09-10 13:44:20 +0800

    board: fsl: lx2160ahpcsom: remove unused I2C mutiplexer code
    Remove unused I2C mutiplexer code.
    
    Signed-off-by: Yangbo Lu <yangbo.lu@nxp.com>

--- board/freescale/lx2160ahpcsom/lx2160ahpcsom.c
+++ board/freescale/lx2160ahpcsom/lx2160ahpcsom.c
@@ -62,48 +62,6 @@ U_BOOT_DEVICE(nxp_serial1) = {
 	.platdata = &serial1,
 };
 
-int select_i2c_ch_pca9547(u8 ch)
-{
-	int ret;
-
-#ifndef CONFIG_DM_I2C
-	ret = i2c_write(I2C_MUX_PCA_ADDR_PRI, 0, 1, &ch, 1);
-#else
-	struct udevice *dev;
-
-	ret = i2c_get_chip_for_busnum(0, I2C_MUX_PCA_ADDR_PRI, 1, &dev);
-	if (!ret)
-		ret = dm_i2c_write(dev, 0, &ch, 1);
-#endif
-	if (ret) {
-		puts("PCA: failed to select proper channel\n");
-		return ret;
-	}
-
-	return 0;
-}
-
-int select_i2c_ch_pca9547_sec(u8 ch)
-{
-	int ret;
-
-#ifndef CONFIG_DM_I2C
-	ret = i2c_write(I2C_MUX_PCA_ADDR_SEC, 0, 1, &ch, 1);
-#else
-	struct udevice *dev;
-
-	ret = i2c_get_chip_for_busnum(0, I2C_MUX_PCA_ADDR_SEC, 1, &dev);
-	if (!ret)
-		ret = dm_i2c_write(dev, 0, &ch, 1);
-#endif
-	if (ret) {
-		puts("PCA: failed to select proper channel\n");
-		return ret;
-	}
-
-	return 0;
-}
-
 static void uart_get_clock(void)
 {
 	serial0.clock = get_serial_clock();
@@ -200,11 +158,6 @@ int esdhc_status_fixup(void *blob, const char *compat)
 }
 
 #if defined(CONFIG_VID)
-int i2c_multiplexer_select_vid_channel(u8 channel)
-{
-	return select_i2c_ch_pca9547(channel);
-}
-
 int init_func_vid(void)
 {
 	int set_vid;
@@ -260,8 +213,6 @@ int board_init(void)
 #ifdef CONFIG_ENV_IS_NOWHERE
 	gd->env_addr = (ulong)&default_environment[0];
 #endif
-
-	select_i2c_ch_pca9547(I2C_MUX_CH_DEFAULT);
 
 #if defined(CONFIG_FSL_MC_ENET)
 	/* invert AQR113 IRQ pins polarity */
