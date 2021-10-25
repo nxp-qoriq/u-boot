nxp-qoriq/u-boot

commit 7beb5a15bc6767b9bc5009724e3c02fc1699400c

board: fsl: lx2160ahpcsom: support reset signal of M.2 slot
Support reset signal of M.2 slot.

Signed-off-by: Yangbo Lu <yangbo.lu@nxp.com>

arch/arm/include/asm/arch-fsl-layerscape/immap_lsch3.h
@@ -65,6 +65,9 @@
 #define I2C7_BASE_ADDR				(CONFIG_SYS_IMMR + 0x01060000)
 #define I2C8_BASE_ADDR				(CONFIG_SYS_IMMR + 0x01070000)
 #endif
+#define GPIO3_BASE_ADDR				(CONFIG_SYS_IMMR + 0x01320000)
+#define GPIO3_GPDIR_ADDR			(GPIO3_BASE_ADDR + 0x0)
+#define GPIO3_GPDAT_ADDR			(GPIO3_BASE_ADDR + 0x8)
 #define GPIO4_BASE_ADDR				(CONFIG_SYS_IMMR + 0x01330000)
 #define GPIO4_GPDIR_ADDR			(GPIO4_BASE_ADDR + 0x0)
 #define GPIO4_GPDAT_ADDR			(GPIO4_BASE_ADDR + 0x8)

board/freescale/lx2160ahpcsom/lx2160ahpcsom.c
@@ -70,6 +70,12 @@ static void uart_get_clock(void)
 
 int board_early_init_f(void)
 {
+	/* GPIO3[14]/EVT_B[2] is used as reset signal of M.2 slot. Reset here. */
+	out_le32(GPIO3_GPDIR_ADDR, (1 << 14 | in_le32(GPIO3_GPDIR_ADDR)));
+	out_le32(GPIO3_GPDAT_ADDR, (~(1 << 14) & in_le32(GPIO3_GPDAT_ADDR)));
+	udelay(100);
+	out_le32(GPIO3_GPDAT_ADDR, ((1 << 14) | in_le32(GPIO3_GPDAT_ADDR)));
+
 #ifdef CONFIG_SYS_I2C_EARLY_INIT
 	i2c_early_init_f();
 #endif

commit c6ec8e148e8640e83bf7ba41ea484d3f5943dc20

board: fsl: lx2160ahpcsom: configure right GPIO for M.2 slot reset signal
Fix to configure right GPIO for M.2 slot reset signal.

Fixes: 7beb5a15bc67 ("board: fsl: lx2160ahpcsom: support reset signal of M.2 slot")
Signed-off-by: Yangbo Lu <yangbo.lu@nxp.com>

arch/arm/include/asm/arch-fsl-layerscape/immap_lsch3.h
@@ -2,7 +2,7 @@
 /*
  * LayerScape Internal Memory Map
  *
- * Copyright 2017-2020 NXP
+ * Copyright 2017-2021 NXP
  * Copyright 2014 Freescale Semiconductor, Inc.
  */
 
@@ -68,6 +68,7 @@
 #define GPIO3_BASE_ADDR				(CONFIG_SYS_IMMR + 0x01320000)
 #define GPIO3_GPDIR_ADDR			(GPIO3_BASE_ADDR + 0x0)
 #define GPIO3_GPDAT_ADDR			(GPIO3_BASE_ADDR + 0x8)
+#define GPIO3_GPIBE_ADDR			(GPIO3_BASE_ADDR + 0x18)
 #define GPIO4_BASE_ADDR				(CONFIG_SYS_IMMR + 0x01330000)
 #define GPIO4_GPDIR_ADDR			(GPIO4_BASE_ADDR + 0x0)
 #define GPIO4_GPDAT_ADDR			(GPIO4_BASE_ADDR + 0x8)

board/freescale/lx2160ahpcsom/lx2160ahpcsom.c
@@ -71,10 +71,12 @@ static void uart_get_clock(void)
 int board_early_init_f(void)
 {
 	/* GPIO3[14]/EVT_B[2] is used as reset signal of M.2 slot. Reset here. */
-	out_le32(GPIO3_GPDIR_ADDR, (1 << 14 | in_le32(GPIO3_GPDIR_ADDR)));
-	out_le32(GPIO3_GPDAT_ADDR, (~(1 << 14) & in_le32(GPIO3_GPDAT_ADDR)));
+	out_le32(GPIO3_GPDIR_ADDR, (1 << 17 | in_le32(GPIO3_GPDIR_ADDR)));
+	out_le32(GPIO3_GPIBE_ADDR, 0xffffffff);
+
+	out_le32(GPIO3_GPDAT_ADDR, (~(1 << 17) & in_le32(GPIO3_GPDAT_ADDR)));
 	udelay(100);
-	out_le32(GPIO3_GPDAT_ADDR, ((1 << 14) | in_le32(GPIO3_GPDAT_ADDR)));
+	out_le32(GPIO3_GPDAT_ADDR, ((1 << 17) | in_le32(GPIO3_GPDAT_ADDR)));
 
 #ifdef CONFIG_SYS_I2C_EARLY_INIT
 	i2c_early_init_f();